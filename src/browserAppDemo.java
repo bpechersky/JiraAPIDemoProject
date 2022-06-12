6f3477293bee803d66670cec11e5915469a5754f
bpechersky <bpechersky@workmail.com>
2022-06-11 22:10:31 -0700

    adding code from selenium course

---
+++ src/browserAppDemo.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
+public class browserAppDemo {
+    public static void main(String[] args) throws Exception
+    {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.setExperimentalOption("useAutomationExtension", false);
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        WebDriver driver =  new ChromeDriver(options);
+        String url = "https://accounts.google.com/signin";
+        driver.get(url);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement email_phone = driver.findElement(By.xpath("//input[@id='identifierId']"));
+        email_phone.sendKeys("bpechersky@gmail.com");
+        driver.findElement(By.id("identifierNext")).click();
+        WebElement password = driver.findElement(By.xpath("//input[@name='password']"));
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(password));
+        password.sendKeys("Budman1967!!!");
+        driver.findElement(By.id("passwordNext")).click();
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+}
--- src/demo/oAuthTest.java
+++ src/demo/oAuthTest.java
@@ -1,14 +1,34 @@
 package demo;
 
 import io.restassured.path.json.JsonPath;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static io.restassured.RestAssured.given;
 
 public class oAuthTest {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
+/*        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&auth_url=https://accounts.google.com/o/oauth2/v2/auth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https://rahulshettyacademy.com/getCourse.php");
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("input[type='email']")).sendKeys("bpechersky@gmail.com");
+        driver.findElement(By.cssSelector("input[type='email']")).sendKeys(Keys.ENTER);
+        Thread.sleep(4000);
+        driver.findElement(By.cssSelector("input[type='password']")).sendKeys("Budman1967!!!");
+        driver.findElement(By.cssSelector("input[type='password']")).sendKeys(Keys.ENTER);
+        String url = driver.getCurrentUrl();*/
+/*        String url  = "https://rahulshettyacademy.com/getCourse.php?code=4%2F0AX4XfWjhr-YD_mh16oWWcwlSIp83qQlno6-AiJjAwn3s5vEdJZQPI9rZgxw0qvBrf1lKQw&scope=email+openid+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&authuser=0&prompt=none";
+        String partialString  =  url.split("code=")[1];
+        String code = partialString.split("&scope")[0];
+        System.out.println(code);*/
+        String code = "4%2F0AX4XfWhBXmeIVsScuvtFv5hFTyaOVx7hrbmfpUF_nNJ3fiGLUeBIeDwY4IV9SqhnWIPsoA";
 
 
-        String accessTokenRespons = given()
+        String accessTokenRespons = given().urlEncodingEnabled(false)
+                .queryParam("code",code)
                 .queryParam("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
                 .queryParam("client_secret","erZOWM9g3UtwNRj340YYaK_W")
                 .queryParam("redirect_uri","https://rahulshettyacademy.com/getCourse.php")
